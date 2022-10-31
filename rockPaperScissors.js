2022102eb6405b7f36f843566984fc8d46639f93
Michael Vandivier <just1rndmkid@gmail.com>
2022-10-30 21:52:08 -0400

    Add Player selection function to rockPaperScissors.js

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,23 +1,43 @@
 // let roll = 1
 // let hand = ""
 
+// getComputerChoice is a function that uses the random module to
+// randomly select a number between 1-3 in order to select a choice of Rock Paper or Scissors
+
 function getComputerChoice()
     {
     let roll = Math.floor(Math.random() * 4);  
     
     if(roll === 1){
         hand ="rock";
-        return hand;
+        return computerHand;
     } else if(roll === 2){
         hand = "paper";
-        return hand;
+        return computerHand;
     } else if (roll === 3){
         hand = "scissors";
-        return hand;
+        return computerHand;
     } else{
         console.log("Please reroll!");
     }
     }
 
 
-console.log(getComputerChoice())
+console.log(getComputerChoice());
+
+function playerSelection(playerInput)
+
+    {
+    if(playerInput === 1){
+        hand ="rock";
+        return playerHand;
+    } else if(roll === 2){
+        hand = "paper";
+        return playerHand;
+    } else if (roll === 3){
+        hand = "scissors";
+        return playerHand;
+    } else{
+        console.log("Please reroll!");
+    }
+    }
